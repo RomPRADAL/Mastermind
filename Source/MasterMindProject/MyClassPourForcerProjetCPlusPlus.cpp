a283a4d8db4728f5aff0da45e5f0b7dbffcb0b6a
RomPRADAL <romain_pradal@orange.fr>
2025-01-20 16:49:24 +0100

    Conversion C++ et plugin rider

---
+++ Source/MasterMindProject/MyClassPourForcerProjetCPlusPlus.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyClassPourForcerProjetCPlusPlus.h"
+
+MyClassPourForcerProjetCPlusPlus::MyClassPourForcerProjetCPlusPlus()
+{
+}
+
+MyClassPourForcerProjetCPlusPlus::~MyClassPourForcerProjetCPlusPlus()
+{
+}
